5a711e6a59a44feabddd725acb0bb6acfacb6329
GorshkovN <gorshov.hikita7@mail.ru>
2025-08-02 12:53:11 +0300

    добавление ellipse по клику

--- CanvasPoints/ViewModels/MainWindowViewModel.cs
+++ CanvasPoints/ViewModels/MainWindowViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.ObjectModel;
 
 
+
 namespace CanvasPoints.ViewModels
 {
     public partial class MainWindowViewModel : ViewModelBase
--- CanvasPoints/Views/MainWindow.axaml.cs
+++ CanvasPoints/Views/MainWindow.axaml.cs
@@ -1,4 +1,10 @@
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.Shapes;
+using Avalonia.Input;
+using CanvasPoints.ViewModels;
+using System;
+using System.Collections.ObjectModel;
 
 namespace CanvasPoints.Views;
 
@@ -7,5 +13,15 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        DataContext = new MainWindowViewModel();
+    }
+    public void CanvasPointerPressed(object sender, PointerPressedEventArgs e)
+    {
+        Point point = e.GetPosition(sender as Control);
+
+        var viewModel = (MainWindowViewModel)DataContext;
+
+        //ObservableCollection<MyEllipse> EllipseList = viewModel.EllipseList;
+        viewModel.EllipseList.Add(new MyEllipse(Convert.ToInt32(point.X)-15, Convert.ToInt32(point.Y)-15));
     }
 }
