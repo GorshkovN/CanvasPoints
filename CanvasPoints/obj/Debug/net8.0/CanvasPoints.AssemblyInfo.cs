6551c8adf837142ffd37eea9ee8e5a693dadc5df
GorshkovN <gorshov.hikita7@mail.ru>
2025-08-03 11:28:13 +0300

    работает

--- CanvasPoints/obj/Debug/net8.0/CanvasPoints.AssemblyInfo.cs
+++ CanvasPoints/obj/Debug/net8.0/CanvasPoints.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CanvasPoints")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e67940f079b5c0c9f42e8f4f0ffac82b39d27df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a711e6a59a44feabddd725acb0bb6acfacb6329")]
 [assembly: System.Reflection.AssemblyProductAttribute("CanvasPoints")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CanvasPoints")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
